709c2cbd3c09336f23b843256128ce7b0b12d7bd
Chandler <ctuggles5@gmail.com>
2026-04-22 01:31:44 -0400

    First Flask page working

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello from aws interview tracker")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>AWS Interview Tracker</h1><p>Your app is alive.</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
